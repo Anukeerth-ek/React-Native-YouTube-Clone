6fa4860498d8a794a44b863e6e1b083b7653b502
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2024-04-01 23:48:46 +0530

    Implemented Rapid Api

---
+++ components/VideoCard.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { View, Image, Text } from 'react-native'
+import { formatViews } from '../utils/Numbers';
+import * as Icon from 'react-native-feather'
+
+const VideoCard = ({video}) => {
+  return (
+    <View className="mt-4">
+    <Image source={video.thumbnail} className="h-52 w-full"/>
+    <View className="flex items-end mr-2 mb-5 -mt-6">
+      <View className="bg-black rounded "> 
+          <Text className="text-white font-semibold text-xs">
+              {video.lengthText}
+          </Text>
+      </View>
+      
+    </View>
+    <View 
+      className="flex-row justify-between items-center pb-5 space-x-3 mx-2">
+          <Image source={video.channelThumbnail} className="h-9 w-9 rounded-full" />
+         
+          <View className="flex-1 space-y-1">
+              <Text className="text-white font-semibold">
+                  {video.title}
+              </Text>
+              <Text className="text-zinc-400 text-xs">
+                  {video.channelTitle.length>20? video.channelTitle.slice(0,20)+'...': video.channelTitle} • {formatViews(video.viewCount)} views • {video.publishedText}
+              </Text>
+          </View>
+          <View className="self-start">
+              <Icon.MoreVertical stroke="white" strokeWidth={2} height={15} />
+          </View>
+    </View>
+  </View>
+  )
+}
+
+export default VideoCard
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,19 +1,32 @@
 import { View, Text, SafeAreaView, Image, ScrollView, TouchableOpacity } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import * as Icon from "react-native-feather";
 
 // IMPORTING IMAGES HERE
 import youtubeIcon from "../assets/icons/youtube_icon.png";
-import shortsIcon from '../assets/icons/shorts_icon.png'
+import shortsIcon from "../assets/icons/shorts_icon.png";
 // import avatar from "../assets/profile.jpg";
-import { categories, shortVideos } from "../constants/index";
+import { categories, shortVideos, youtubeVideos } from "../constants/index";
 import ShortVideoCard from "../components/ShortVideoCard";
-
+import VideoCard from "../components/VideoCard";
+import { fetchTrendingVideo, videoHandler } from "../youtubeApi";
 
 export default function HomeScreen() {
+
+  useEffect(()=> {
+     fetchData();
+  }, [])
+
+  const fetchData = async ()=> {
+     const data = await fetchTrendingVideo();
+     console.log("videos", data[0])
+     setVideos(data)
+  }
      // USESTATES HERE
-     const [activeCategory, setActiveCategory] = useState('All')
+     const [activeCategory, setActiveCategory] = useState("All");
+     const [videos, setVideos] = useState([])
+
      return (
           <View style={{ backgroundColor: "#212121", minHeight: "100%" }}>
                <SafeAreaView className="my-10 mx-3">
@@ -22,57 +35,62 @@ export default function HomeScreen() {
                          {/* ICON AND BRAND AREA */}
                          <View className="flex-row items-center  space-x-2">
                               <Image source={youtubeIcon} className="h-7 w-10" />
-                              <Text className="text-white font-semibold text-xl tracking-tighter">YouTube </Text>
+                              <Text className="text-white font-semibold text-xl tracking-tighter">MyTube </Text>
                          </View>
-                         <View className="flex-row items-center space-x-2">
+                         <View className="flex-row items-center space-x-5">
                               <Icon.Cast stroke="white" strokeWidth={1.2} height={22} />
                               <Icon.Bell stroke="white" strokeWidth={1.2} height={22} />
                               <Icon.Search stroke="white" strokeWidth={1.2} height={22} />
-                             
+                              {/* <Icon.Home stroke="white" strokeWidth={1.2} height={22} /> */}
+                              
 
                               {/* <Image source={avatar} className="w-7 h-7 rounded-full object-cover" /> */}
                          </View>
                     </View>
-                    </SafeAreaView>
+               </SafeAreaView>
 
-
-                    
-                    {/* CATEGORY  SECTION */}
-                         <ScrollView className=" flex-1 -mt-6" showsVerticalScrollIndicator={false}>
+               {/* CATEGORY  SECTION */}
+               <ScrollView className=" flex-1 -mt-6" showsVerticalScrollIndicator={false}>
                     <View className="py-2 pb-5 ">
                          <ScrollView className="px-4" horizontal showsHorizontalScrollIndicator={false}>
-                              {categories.map((category, index)=>{
+                              {categories.map((category, index) => {
                                    let isActive = category == activeCategory;
-                                   let textClass = isActive ? 'text-black' : 'text-white'
-                                   return(
-                                        <TouchableOpacity key={index} 
-                                        style={{backgroundColor: isActive ? 'white' : 'rgba(255, 255, 255, 0.1)'}}
-                                         className="rounded-md p-1 px-3 mr-2" onPress={()=> setActiveCategory(category)}>
+                                   let textClass = isActive ? "text-black" : "text-white";
+                                   return (
+                                        <TouchableOpacity
+                                             key={index}
+                                             style={{ backgroundColor: isActive ? "white" : "rgba(255, 255, 255, 0.1)" }}
+                                             className="rounded-md p-1 px-3 mr-2"
+                                             onPress={() => setActiveCategory(category)}
+                                        >
                                              <Text className={textClass}>{category}</Text>
                                         </TouchableOpacity>
-                                   )
+                                   );
                               })}
-                              </ScrollView>
-                       
+                         </ScrollView>
                     </View>
 
                     {/* SHORTS SECTIONS */}
                     <View className="px-2 space-y-3 ">
                          <View className="mx-1 flex-row items-center space-x-2">
-                              <Image source={shortsIcon } className="h-6 w-5"/>
+                              <Image source={shortsIcon} className="h-6 w-5" />
                               <Text className="text-white font-semibold text-lg ">Shorts</Text>
                          </View>
-                              <ScrollView horizontal showsHorizontalScrollIndicator={false} className="px-3">
-                                   {
-                                        shortVideos.map((item, index)=> <ShortVideoCard item={item} key={index}/>)
-                                   }
-                              </ScrollView>
+                         <ScrollView horizontal showsHorizontalScrollIndicator={false} className="px-3">
+                              {shortVideos.map((item, index) => (
+                                   <ShortVideoCard item={item} key={index} />
+                              ))}
+                         </ScrollView>
                     </View>
 
+                    {/* VIDEOS SECTION */}
 
-
-
+                    <ScrollView showsVerticalScrollIndicator={false}>
+                         {
+                            videos.map((video, index)=> <VideoCard video={video} key={index}/>)  
+                         }
                     </ScrollView>
+               </ScrollView>
           </View>
      );
 }
