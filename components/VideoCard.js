1886ae7847784928ef4ffae67e375ddfad14e682
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2024-05-02 14:13:17 +0530

    Altered VideoCard.jsx

--- components/VideoCard.js
+++ components/VideoCard.js
@@ -5,29 +5,30 @@ import * as Icon from 'react-native-feather'
 
 
 const VideoCard = ({video}) => {
+  console.log("vide", video)
   return (
     <View className="mt-4">
       
-    <Image  source={{ uri: video.thumbnail[0].url }}  className="h-52 w-full"/>
+    <Image  source={{ uri: video?.thumbnail[0].url }}  className="h-52 w-full"/>
     <View className="flex items-end mr-2 mb-5 -mt-6">
       <View className="bg-black rounded "> 
           <Text className="text-white font-semibold text-xs">
-              {video.lengthText}
+              {video?.lengthText}
           </Text>
       </View>
       
     </View>
     <View 
       className="flex-row justify-between items-center pb-5 space-x-3 mx-2">
-     <Image source={{ uri: video.channelThumbnail[0].url }} className="h-9 w-9 rounded-full" />
+     <Image source={{ uri: video?.channelThumbnail[0].url }} className="h-9 w-9 rounded-full" />
 
          
           <View className="flex-1 space-y-1">
               <Text className="text-white font-semibold">
-                  {video.title}
+                  {video?.title}
               </Text>
               <Text className="text-zinc-400 text-xs">
-                  {video.channelTitle.length>20? video.channelTitle.slice(0,20)+'...': video.channelTitle} • {formatViews(video.viewCount)} views • {video.publishedText}
+                  {video?.channelTitle.length>20? video?.channelTitle.slice(0,20)+'...': video?.channelTitle} • {formatViews(video?.viewCount)} views • {video?.publishedText}
               </Text>
           </View>
           <View className="self-start">
--- youtubeApi/api.js
+++ youtubeApi/api.js
@@ -13,12 +13,13 @@ export  const fetchTrendingVideo = async (params)=> {
           ...params
         },
         headers: {
-          'X-RapidAPI-Key': rapidApiKey,
+          'X-RapidAPI-Key': "f19b8ca4e4msh21731cb0b4b6ba1p18a59djsnf28748692450",
           'X-RapidAPI-Host': 'youtube-v3-alternative.p.rapidapi.com'
         }
       };
       try {
         const response = await axios.request(options);
+        console.log("response", response.data.data)
         return response.data.data
       }
        catch(error) {
